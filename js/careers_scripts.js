04004c272333dabe7e65403cdb1fa2f35b94dda7
kachanovskyi <vova.kachanovsky@gmail.com>
2017-05-03 14:57:21 +0300

    added Den. Removed sales vacancy. Added QA vacancy.

--- js/careers_scripts.js
+++ js/careers_scripts.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     $(window).resize(function () {
-        if(matchMedia('(max-width:767px)').matches && $("#modalSalesManager").data('bs.modal').isShown) {
+        if(matchMedia('(max-width:767px)').matches && $("#modalQA").data('bs.modal').isShown) {
             $('.modal-mobile-header').css('display', 'block');
         } else {
             $('.modal-mobile-header').css('display', 'none');
         }
     });
 
-    $('#roleSalesManager').on("click", function () {
+    $('#roleQA').on("click", function () {
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-            window.open("careers/project-manager.html");
+            window.open("careers/qa.html");
         } else {
-            $('#modalSalesManager').modal('show');
+            $('#modalQA').modal('show');
             autosize($('textarea'));
         }
     });
@@ -24,16 +24,16 @@ $(document).ready(function () {
         }
     });
 
-    $('#roleSalesManager, #roleJavaDev').on("click", function () {
+    $('#roleQA, #roleJavaDev').on("click", function () {
         if(matchMedia('(max-width:767px)').matches) {
             $('.modal-mobile-header').css('display', 'block');
         }
     });
     $('#modalCloseBtn').on("click", function () {
-        $('#modalSalesManager, #modalJavaDev').modal('hide');
+        $('#modalQA, #modalJavaDev').modal('hide');
         $('.modal-mobile-header').fadeOut("fast");
     });
-    $('#modalSalesManager, #modalJavaDev').on('hidden.bs.modal', function() {
+    $('#modalQA, #modalJavaDev').on('hidden.bs.modal', function() {
         $('.modal-mobile-header').css('display', 'none');
     });
 });
